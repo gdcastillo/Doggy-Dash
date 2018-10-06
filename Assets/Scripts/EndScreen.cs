e012c51a99768f168277822e77b98e4b849b0d6f
gdcastillo <gdcastillo@ufl.edu>
2018-10-06 10:40:07 -0400

    Enemy AI Scripts Finalized

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour {
+
+	private void Update ()
+	{
+		if (Input.anyKeyDown) 
+		{
+			Application.Quit ();
+			Debug.Log ("any key pressed");
+		}
+	}
+
+}
---
+++ Assets/Scripts/UIInput.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIInput : MonoBehaviour 
+{
+	[Header ("Audio")]
+
+	[SerializeField]
+	private AudioSource ButtonSound;
+
+	[SerializeField]
+	private GameObject howtoplayPopUp;
+	[SerializeField]
+	private GameObject aboutPopUp;
+
+	void Start ()
+	{
+		howtoplayPopUp.SetActive (false);
+		aboutPopUp.SetActive (false);
+	}
+
+	public void OnTriggerDown()
+	{
+		ButtonSound.Play ();
+	}
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+		Debug.Log ("Closed");
+	}
+
+	public void HowToPlayPopUp()
+	{
+		howtoplayPopUp.SetActive (true);
+	}
+
+	public void AboutPopUp()
+	{
+		aboutPopUp.SetActive (true);
+	}
+		
+}
